5bd7889e5ed531e083fc03c3c127e22d3adf0375
ThaliaDiaz <thalidiaz24@gmail.com>
2017-11-20 04:46:29 -0500

    version 0.0.1

---
+++ js/app.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function() {
+/* Definiendo variables*/
+  var input = document.getElementById('input');
+  var container = document.getElementById('container');
+  
+
+  /* Mostrando el form cuando le doy clic al botón*/
+  input.addEventListener('click', function(event) {
+    input.value = '';
+    var div = document.getElementById('board');
+    var button = document.getElementById('button');
+    var close = document.getElementById('cross');
+    div.setAttribute('class', 'board-insert-list');
+    input.setAttribute('class', 'list-name-input insert-list-input');
+    button.setAttribute('class', 'button-insert-list');
+    close.setAttribute('class', 'fa fa-times');
+  });
+});
